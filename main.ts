ed2b4e276a28c123519020239c544bb330d1cd18
ShaistaKhan5 <150230170+ShaistaKhan5@users.noreply.github.com>
2024-02-19 09:57:37 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+
+let user = await inquirer .prompt([
+    {
+        type: "input",
+        name: "UserName",
+        message: "what is your name?",
+        validate:(ans)=>{
+            if(!ans){
+                return "please give your answer"
+            }
+            return true
+        }
+    },
+
+    {
+        type: "number",
+        name: "userAge",
+        message: "what is your age?"
+
+    },
+     {
+        type:"list",
+        name:"userHobby",
+        message: "what is your Hobby?",
+        choices:["cricket", "study", "learning",]
+     },
+     {
+        type: "checkbox",
+        name: "userEmail",
+        messsage: "what is your Email",
+        choices:["Etc@test.com", "Day@test.com", "Shy@test.com"],
+        default: "Nothing"
+     },
+    ])
+
+    console.log(user)
+
+
+
+
+
